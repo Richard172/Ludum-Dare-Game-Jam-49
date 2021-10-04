bde8c91de64cb5e41cdd85d30cf8d0f2f340d1a9
Richard Jiang <jiang1774624096@gmail.com>
2021-10-03 17:16:47 -0700

    added full screen toggles

--- LudumDare49/scenes/GameManager.gd
+++ LudumDare49/scenes/GameManager.gd
@@ -2,3 +2,10 @@ extends Node
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _input(event):	
+	if event.is_action_pressed("toggle_fullscreen"):
+		if OS.window_fullscreen == true:
+			OS.window_fullscreen = false;
+		else:
+			OS.window_fullscreen = true;
